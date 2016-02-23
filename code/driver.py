d288bd8e9302e72c1cd84ef714b79c3e794d7a63
Matt May <matthewbentonmay@gmail.com>
2016-02-23 17:53:58 -0600

    modified a few filenames for clarity

--- code/driver.py
+++ code/driver.py
@@ -6,8 +6,8 @@ type_map = { 'sidewalk': 1, 'crosswalk': 2, 'entrance': 3, 'exit': 4 }
 
 # Create a grid object that contains the underlying nodes and path information.
 grid = Grid({
-    'node_file': 'Map_GATech_Stadium.png.vertex',
-    'edge_file': 'Map_GATech_Stadium.png.edge',
+    'node_file': 'nodes.csv',
+    'edge_file': 'edges.csv',
     'type_map': type_map,
     'paths_file': 'paths_gatech.pickle'
     #'new_paths_file': 'paths_gatech.pickle'
@@ -17,7 +17,7 @@ grid = Grid({
 simulation = Simulation(grid, {
     'num_pedestrians': 500,
     'visualization': True,
-    'vis_image': 'Map_GATech_Stadium.png'
+    'vis_image': 'map.png'
 })
 
 # Run the simulation.
