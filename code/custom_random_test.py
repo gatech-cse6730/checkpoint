0d74777250ff82654167e0949d7f7c69f58ceab4
Matt May <matthewbentonmay@gmail.com>
2016-02-25 22:02:06 -0600

    working on rng test

---
+++ code/custom_random_test.py
@@ -0,0 +1,24 @@
+from custom_random import CustomRandom
+import numpy as np
+
+class CustomRandomTest():
+    """
+    Tests a custom random number generator for use in simulation.
+    """
+
+
+    """
+    Creates a new CustomRandomTest object.
+
+    Args:
+      None.
+
+    Returns:
+      A new CustomRandomTest object.
+
+    """
+    def __init__(self, n_samples):
+        pass
+
+    def test(self):
+        pass
