bd8570457db75bea8d04d3b1578d8c9fcd4f35e0
matt may <matthewbentonmay@gmail.com>
2016-03-02 02:15:22 +0000

    added logs, results from server 2

--- code/runme.sh
+++ code/runme.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-python sim_batch.py -c config/config1.json -p 55000 -v f -f ./paths/paths_2016-02-22.pickle >log1.log 2>&1 &
-python sim_batch.py -c config/config4.json -p 55000 -v f -f ./paths/paths_2016-02-22.pickle >log2.log 2>&1 &
-python sim_batch.py -c config/config7.json -p 55000 -v f -f ./paths/paths_2016-02-22.pickle >log3.log 2>&1 &
-python sim_batch.py -c config/config8.json -p 55000 -v f -f ./paths/paths_2016-02-22.pickle >log4.log 2>&1 &
-python sim_batch.py -c config/config2.json -p 55000 -v f >log5.log 2>&1 &
+#python sim_batch.py -c config/config3.json -p 55000 -v f >log1.log 2>&1 &
+#python sim_batch.py -c config/config5.json -p 55000 -v f >log2.log 2>&1 &
+#python sim_batch.py -c config/config6.json -p 55000 -v f >log3.log 2>&1 &
+#python sim_batch.py -c config/config9.json -p 55000 -v f >log4.log 2>&1 &
+python sim_batch.py -c config/config2.json -p 55000 -v f -f config2.pickle >config2.log 2>&1 &
+python sim_batch.py -c config/config5.json -p 55000 -v f -f config5.pickle >config5.log 2>&1 &
+python sim_batch.py -c config/config10.json -p 55000 -v f -f config10.pickle >config10.log 2>&1 &
